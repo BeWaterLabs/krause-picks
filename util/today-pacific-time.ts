f581bc6773445cc18d7d7d1def7d9afe533259cf
gregfromstl <gregfromstl@gmail.com>
2023-10-24 17:36:39 -0700

    replaces moment with dayjs

--- util/today-pacific-time.ts
+++ util/today-pacific-time.ts
@@ -1,11 +1,17 @@
-import moment from "moment-timezone";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
 export default function todayPacificTime() {
-    const startOfTodayPT = moment()
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+
+    const startOfTodayPT = dayjs()
         .tz("America/Los_Angeles")
         .startOf("day")
         .toDate();
-    const endOfTodayPT = moment()
+
+    const endOfTodayPT = dayjs()
         .tz("America/Los_Angeles")
         .endOf("day")
         .toDate();
