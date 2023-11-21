e01cfc5639642ef31fa865f11a2be15928e5abde
gregfromstl <gregfromstl@gmail.com>
2023-11-21 09:05:37 -0800

    adds counter

--- components/UserPanelPicks.tsx
+++ components/UserPanelPicks.tsx
@@ -13,8 +13,16 @@ export default function UserPanelPicks({
 }) {
     return (
         <div className="absolute top-0 flex flex-col min-h-full right-0 left-0">
-            <h3 className="text-xl mt-6 font-heading dark:text-white text-black font-semibold">
+            <h3 className="text-xl flex justify-between items-center mt-6 font-heading dark:text-white text-black font-semibold">
                 Quick Picks
+                <p className="text-sm mt-2 font-body dark:text-gray-400 text-gray-600">
+                    {
+                        picks.filter((p) =>
+                            games.some((g) => g.id === p.game.id)
+                        ).length
+                    }{" "}
+                    / {games.length} picks made
+                </p>
             </h3>
             <div className="flex flex-col gap-4 mt-3">
                 {games.map((game) => {
@@ -29,14 +37,21 @@ export default function UserPanelPicks({
                     );
                 })}
             </div>
-            {picks.filter((p) => p.successful === null).length > 0 && (
+            {picks.filter(
+                (p) =>
+                    p.successful === null && new Date(p.game.start) > new Date()
+            ).length > 0 && (
                 <>
                     <h3 className="text-xl mt-8 font-heading dark:text-white text-black font-semibold">
                         Pending Picks
                     </h3>
                     <div className="flex flex-col gap-4 mt-3">
                         {picks
-                            .filter((p) => p.successful === null)
+                            .filter(
+                                (p) =>
+                                    p.successful === null &&
+                                    new Date(p.game.start) < new Date()
+                            )
                             .map((pick) => (
                                 <FinalizedPick key={pick.id} pick={pick} />
                             ))}
